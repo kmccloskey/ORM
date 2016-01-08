a6e9be21bb4aec4c6b27d253217d65f9b56b696d
Kyle McCloskey <mccloskeykyle@gmail.com>
2016-01-07 19:23:47 -0500

    It works

--- write_orm.rb
+++ write_orm.rb
@@ -1,5 +1,6 @@
 # Kyle McCloskey - Bare ORM 
 class User
+	# create db homeworktest
 	# Before using this code, have this table setup in DB
 	# CREATE TABLE users(
 	# id serial PRIMARY KEY,
@@ -7,50 +8,82 @@ class User
 	# 	lname VARCHAR(50),
 	# 	email VARCHAR(50)
 	# );
-attr_accessor :fname, :lname, :email
+	attr_accessor :fname, :lname, :email
 
-def initialize(fname="", lname="", email="")
-	puts "HELLO"
-	@fname = fname
-	@lname = lname
-	@email = email
-end
+	def initialize(fname="", lname="", email="")
+		@fname = fname
+		@lname = lname
+		@email = email
+	end
 
-def save
-	`psql -d homeworktest -c "INSERT INTO users (fname, lname, email) VALUES ('#{@fname}', '#{@lname}', '#{@email}')"`
-end
+	# Used to create a new users in the database
+	def self.save(fname="", lname="", email="")
+		@fname = fname
+		@lname = lname
+		@email = email
+		# Takes the arguements passed into save and adds them into the database by arg value
+		`psql -d homeworktest -c "INSERT INTO users (fname, lname, email) VALUES ('#{@fname}', '#{@lname}', '#{@email}')"`
+		puts "You created a new User"
+	end
 
-def self.find(id)
-	command = `psql -d homeworktest -c "SELECT * FROM users WHERE id = #{id}"`
-	puts sql_results_parser(command)
-end
+	# Used to find a user by id
+	def self.find(id)
+		# searches the database who's id is = to the arguement entered
+		command = `psql -d homeworktest -c "SELECT * FROM users WHERE id = #{id}"`
+		# sets the result to x
+		x = sql_results_parser(command)
+		# returns x
+		x[0]
+	end
 
-def self.where(fname)
-	command = `psql -d homeworktest -c "SELECT * FROM users WHERE fname = '#{fname}'"`
-	puts sql_results_parser command
-end
+	# Searches for a user by their first name
+	def self.where(fname)
+		command = `psql -d homeworktest -c "SELECT * FROM users WHERE fname = '#{fname}'"`
+		x = sql_results_parser(command)
+		x[0]
+	end
 
-def self.all
-	command = `psql -d homeworktest -c "SELECT * FROM users"`
-	puts sql_results_parser(command)
-end
+	# Shows all the users in the database
+	def self.all
+		command = `psql -d homeworktest -c "SELECT * FROM users"`
+		sql_results_parser(command)
+	end
 
-def self.last
-	command = `psql -d homeworktest -c "SELECT users FROM users ORDER BY id DESC LIMIT 1;"`
-	puts sql_results_parser command
-end
+	# Shows the last user in the databse
+	def self.last
+		command = `psql -d homeworktest -c "SELECT users FROM users ORDER BY id DESC LIMIT 1;"`
+		x = sql_results_parser(command)
+		x[0]
+	end
 
-def self.first
-	command = `psql -d homeworktest -c "SELECT users FROM users ORDER BY id ASC LIMIT 1;"`
-	puts sql_results_parser command
-end
+	# Shows the first user in the database
+	def self.first
+		command = `psql -d homeworktest -c "SELECT users FROM users ORDER BY id ASC LIMIT 1;"`
+		x = sql_results_parser(command)
+		x[0]
+	end
 
-def self.sql_results_parser(command)
+	#Deletes all users from the database
+	def self.destroy_all
+		`psql -d homeworktest -c "DELETE FROM users"`
+		puts "You've deleted all users from the database"
+	end
+
+	#Deletes a user by it's id
+	def self.destroy(id)
+		# searches the database who's id is = to the arguement entered
+		`psql -d homeworktest -c "DELETE FROM users WHERE id = #{id}"`
+		puts "You deleted User# #{id}"
+	end
+
+	# All other commands feed to this method to parse the information
+	def self.sql_results_parser(command)
 		# Goes through each line of how the table would be represented in the console by \n
-		# Sets num to zero
-		num = 0
 		# Sets the variable array1 to an empty array that will be filled later
 		array1 = []
+
+		# Sets num to zero
+		num = 0
 		# Loops through the results fed in from the SQL Command
 		command.each_line do |line|
 			# Increments num from 0 to 1 and will continue with each loop through
@@ -60,22 +93,14 @@ def self.sql_results_parser(command)
 			# sets variable @a to line.split breaking each individual string apart and putting it on the next line
 			@a = line.split
 			# if the num is < 3 and the length of @a is less then four skip and go to the next line
-			if num < 3 || @a.count <4
+			if num < 3 || @a.count < 4
 				# Skips the next line
 				next
 			end
 			# sets the variable u to the contents of it's index (fname, lname, email)
-       		u = User.new(@a[1], @a[2], @a[3])
-       		# prints all three
-       		# puts u.fname
-       		# puts u.lname
-       		# puts u.email
-       		save(u)
-       		array1.push(line)
-        end
-        puts array1
-        return nil
-    	
-    end
-
+	      	u = User.new(@a[1], @a[2], @a[3])
+	       	array1.push(u)
+	        end
+	    return array1
+	end
 end
